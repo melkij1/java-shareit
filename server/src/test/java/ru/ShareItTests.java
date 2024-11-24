8361a98510ca55817bc0b2430e35f1997f93f6c3
mmsobakin <mmsobakin@spasibosb.ru>
2024-11-24 15:34:51 +0300

    fix checkstyle v2

--- server/src/test/java/ru/ShareItTests.java
+++ server/src/test/java/ru/ShareItTests.java
@@ -1,13 +1,9 @@
 package ru;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ShareItTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
