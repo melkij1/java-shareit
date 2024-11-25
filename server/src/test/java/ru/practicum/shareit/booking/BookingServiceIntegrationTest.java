54ce36aa1d925bba1dac0fc5377ac413ac3ef02f
mmsobakin <mmsobakin@spasibosb.ru>
2024-11-25 12:56:48 +0300

    fix checkstyle v3.8

--- server/src/test/java/ru/practicum/shareit/booking/BookingServiceIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingServiceIntegrationTest.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingDtoIn;
